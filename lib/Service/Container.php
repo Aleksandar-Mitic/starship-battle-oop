8649cd3c07da0f9d3097668d03707585736f7815
Aleksandar Mitic <aleksandar.webart@gmail.com>
2019-05-15 14:50:38 +0200

    JsonStorage added & Interface for storage options

--- lib/Service/Container.php
+++ lib/Service/Container.php
@@ -36,11 +36,13 @@ class Container
         return $this->pdo;
     }
 
-
+    /**
+     * @return [ShipStorageInterface]
+     */
     public function getShipStorage()
     {
         if ($this->shipStorage === null) {
-            $this->shipStorage = new PdoShipStorage($this->getPDO());
+            $this->shipStorage = new JsonFileShipStorage(__DIR__.'/../../resources/ships.json');
         }
         return $this->shipStorage;
     }
